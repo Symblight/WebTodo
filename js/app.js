4e3d8361ed0e09681c76e0b65a9dbaf05257be98
Aleksey <symblight@gmail.com>
2016-09-05 16:37:55 +0300

    Update function Edit

--- js/app.js
+++ js/app.js
@@ -47,46 +47,42 @@ function loadStorageValues(){
 }
 
 function addBlockValue(value,idItem, date){	
-	var wall_post="<div class='task_item'><span>"+date+"</span><button class='flat_button' id=del_"+idItem+" title='Удалить из списка'>Delete</button><div class='wall_post_text'><span id='txt_"+idItem+"'>"+value+"</span></div><br><button class='flat_button' id=edit_"+idItem+">Edit</button></div>";
+//	var xmlhttp=new ActiveXObject();
+	var wall_post="<div class='task_item'><span>"+date+"</span><button class='flat_button action_del' id=del_"+idItem+" title='Удалить из списка'>Delete</button><div class='wall_post_text'><span id='txt_"+idItem+"'>"+value+"</span></div><br><button class='flat_button action_edit' id=edit_"+idItem+">Edit</button></div>";
 	var liEl=document.createElement('li');
 	liEl.className='tdItem';
 	liEl.innerHTML=wall_post;
+	liEl.id=idItem;
+	$(liEl).attr("Name","liItem");
 	document.body.appendChild(liEl);
+	$(liEl).show("slow");
+	console.log(liEl.id);
 };
 
-document.querySelectorAll('button').forEach(function(elem) {
-	if (elem.getAttribute('id').substring(0,4)==="del_")
-		elem.onclick = function(event) {
-			var idBtn=elem.getAttribute('id').substring(4);
-			var parentElem=elem.parentElement.parentElement;
-			localStorage.removeItem(idMask+idBtn);
-			console.log(idMask+idBtn);
-			parentElem.remove();
+$('.action_del').click(function(){
+	var liId=$(this).closest('li').attr('id');
+	$(this).closest('li').remove();
+	localStorage.removeItem(idMask+liId);
+})
+
+$('.action_edit').click(function(){
+	var id=$(this).closest('li').attr('id');
+		if (!$(this).hasClass('Save')){
+			$('span#txt_'+id).replaceWith(function(){
+				return $('<textarea id="txt_'+id+'">'+$('span#txt_'+id).html()+'</textarea>')
+			});
+			$(this).html("Save");
+			$(this).addClass('Save');	
+		} else if ($(this).hasClass('Save')){
+					EditLocalStorage(id,$('textarea#txt_'+id).val());
+					$('textarea#txt_'+id).replaceWith(function(){
+						return $('<span id="txt_'+id+'">'+$('textarea#txt_'+id).val()+'</span>')
+					});
+				$(this).html("Edit");
+				$(this).removeClass('Save');
 		};
-	if (elem.getAttribute('id').substring(0,5)==="edit_") editValueTask(elem);
 });
 
-//JQuery
-function editValueTask(e){
-	e.onclick = function(){
-		var idBtn=e.getAttribute('id').substring(5);
-		var span=document.getElementById('txt_'+idBtn);	
-		if (e.innerHTML==='Edit'){
-
-			$('span#txt_'+idBtn).replaceWith(function(){
-				return $('<textarea id="txt_'+idBtn+'">'+$(span).html()+'</textarea>')
-			});
-			e.innerHTML="Save";
-		} else if (e.innerHTML==='Save'){
-			span=document.getElementById('txt_'+idBtn).value;
-		$('textarea#txt_'+idBtn).replaceWith(function(){
-				return $('<span id="txt_'+idBtn+'">'+span+'</span>')
-			});
-			e.innerHTML="Edit";
-			EditLocalStorage(idBtn,span);
-		}
-	};
-}
 
 function EditLocalStorage(id, value){
 	for (key in localStorage){
